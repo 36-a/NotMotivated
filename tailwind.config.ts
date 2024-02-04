cc7dafa26eceb6044d41d8b629bbb7e43a68b619
36-a <36.github@gmail.com>
2024-02-05 03:11:09 +0900

    Tailwind CSS設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,49 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-    content: [
-        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
+    content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+    darkMode: 'class',
     theme: {
-        extend: {
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-            },
+        colors: {
+            main: '#7288CC',
+            darkened: '#545D7B',
+            white: '#F7F8FD',
+            black: '#28292B',
+            link: '#428DD9',
+            code: '#eaf4f7',
         },
+        screens: {
+            sm: '600px',
+            md: '768px',
+            lg: '1280px',
+        },
+        spacing: {
+            '0': '0',
+            '1': '1px',
+            '2': '2px',
+            '3': '3px',
+            '4': '4px',
+            '5': '5px',
+            '8r': '0.5rem',
+            '10': '10px',
+            full: '100%',
+        },
+        fontFamily: {
+            sans: ['var(--NotoSansJp)', 'sans-serif'],
+            code: ['Menlo', 'Monaco'],
+        },
+        fontSize: {
+            xs: ['0.625rem', { lineHeight: '1rem' }], //10px
+            sm: ['0.75rem', { lineHeight: '1rem' }], // 12px
+            base: ['0.875rem', { lineHeight: '1rem' }], // 14px
+            lg: ['1rem', { lineHeight: '1.25rem' }], // 16px
+            xl: ['1.125rem', { lineHeight: '1.25rem' }], // 18px
+        },
+        fontWeight: {
+            normal: '400',
+            bold: '700',
+        },
+        extend: {},
     },
     plugins: [],
 };
